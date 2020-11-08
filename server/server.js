e33e47f24ace10a856c4ff6dbbad8e515f7441e3
Juan Gabriel Sanchez Londoño <juangabrielsanchezlondono@MacBook-Pro-de-Juan.local>
2020-11-08 15:52:08 -0500

    Ajuste en el get usuario

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/usuarios', function (req, res) {
+app.get('/usuario', function (req, res) {
   res.json('Get usuario')
 })
 
