36f61bdc92120654daa23cbc267b411faa1c0513
Juan Gabriel Sanchez Londoño <juangabrielsanchezlondono@MacBook-Pro-de-Juan.local>
2020-11-24 01:12:56 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://gazoo:hiq4YZYSupgwogPn@cafe.x6amk.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
